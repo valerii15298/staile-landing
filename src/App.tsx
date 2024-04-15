fdb519ec7d8d6d849c4494fad6d90f635943efbd
Valerii Petryniak <valerii15298@gmail.com>
2024-04-15 19:27:23 +0300

    add responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import type { Component } from "solid-js";
 import logo from "./assets/favicon.svg";
+import iphone13Icon from "./assets/iphone13.png";
 import { MainList } from "./MainList";
 
 const App: Component = () => {
   return (
     <>
-      <header class="m-10 flex items-center gap-2 text-staile text-3xl font-bold">
-        <img src={logo} alt="STAILE logo" class="max-w-20" />
-        STAILE
-      </header>
-      <main class="my-auto md:max-w-[75%] text-balance">
-        <section class="md:ml-28 mx-10">
-          <h2 class="my-5 font-inter text-6xl font-extrabold text-staile">
-            Знайди свій ідеальний образ за декілька кліків!
-          </h2>
-          <p class="font-mulish my-5 text-2xl">
-            Твій онлайн-помічник, який враховує те, що ти хочеш, для зручного
-            пошуку одягу
-          </p>
-          <button class="my-5 bg-white rounded-3xl p-4 font-extrabold text-2xl">
-            Завантажити додаток
-          </button>
-          <MainList />
-        </section>
-      </main>
+      <div class="flex flex-col h-full w-fit">
+        <header class="m-10 flex items-center gap-2 text-staile text-3xl font-bold">
+          <img src={logo} alt="STAILE logo" class="max-w-20" />
+          STAILE
+        </header>
+        <main class="my-auto text-balance">
+          <section class="xl:ml-28 mx-10">
+            <h2 class="my-5 font-inter text-6xl font-extrabold text-staile">
+              Знайди свій ідеальний образ за декілька кліків!
+            </h2>
+            <p class="font-mulish my-5 text-2xl">
+              Твій онлайн-помічник, який враховує те, що ти хочеш, для зручного
+              пошуку одягу
+            </p>
+            <button class="my-5 bg-white md:bg-blue-700 lg:bg-red-800 xl:bg-orange-500 2xl:bg-lime-600 rounded-3xl p-4 font-extrabold text-2xl">
+              Завантажити додаток
+            </button>
+            <MainList />
+          </section>
+        </main>
+      </div>
+      <img
+        class="-mr-52 xl:-mr-30 mt-10 max-h-[800px] -ml-10 lg:my-auto"
+        src={iphone13Icon}
+      />
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ export default {
       colors: {
         staile: "#3d4455",
       },
+      screens: {
+        "-2xl": { max: "1535px" },
+        "-xl": { max: "1279px" },
+        "-lg": { max: "1023px" },
+        "-md": { max: "767px" },
+        "-sm": { max: "639px" },
+      },
     },
   },
   plugins: [],
